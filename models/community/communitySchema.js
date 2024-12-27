419af9feaf3b75f9b33a6c5571a15d178466c129
jiihyun12 <j22h.h0h@gmail.com>
2024-12-27 23:44:55 +0900

    루트라우터 수정

--- models/community/communitySchema.js
+++ models/community/communitySchema.js
@@ -1,36 +1,21 @@
 import { Schema, model } from "mongoose";
 
+const getCurrentTime = () => new Date().toISOString();
+
+// Community 스키마
 const communitySchema = new Schema({
-    title : { type : String, required : true },
-    author : { type : Schema.Types.ObjectId, ref : 'User' ,required : true },
-    content : { type : String, required : true },
-    category: { 
-      type: String, 
-      enum: ['전체', '뮤지컬', '공연', '영화', '밴드', '연극'],  // 드롭다운 선택 항목
-      required: true 
-    },
-    likeCount : { type : Number, default : 0 },
-    file : { type : String },
-    createdAt: { type: String, default: getCurrentTime },
-    updatedAt: { type: String, default: getCurrentTime },
+  title: { type: String, required: true }, // 제목
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true }, // 작성자 (User 참조)
+  content: { type: String, required: true }, // 내용
+  category: { 
+    type: String, 
+    enum: ["전체", "뮤지컬", "공연", "영화", "밴드", "연극"], // 카테고리 
+    required: true 
+  },
+  likeCount: { type: Number, default: 0 }, // 좋아요 수
+  file: { type: String }, // 파일 경로
+  createdAt: { type: String, default: getCurrentTime }, // 생성 시간
+  updatedAt: { type: String, default: getCurrentTime }, // 업데이트 시간
 });
 
-const communitiesSchema = new Schema(
-  {
-      id: 1,
-      title: "타이틀1",
-      category: "뮤지컬",
-      description: "설명1",
-      imageUrl: "https://web-cf-image.cjenm.com/resize/1344x756/public/share/metamng/programs/kingkyboots-musical-ko-001-06.jpg?v=1732606423", 
-  },
-  {
-    id: 2,
-    title: "타이틀2",
-    category: "공연",
-    description: "설명2",
-    imageUrl: "https://www.sac.or.kr//site/main/file/thumbnail/uu/d5eb6904359c4b5d8eae72d44160e40d", 
-  },
-
-)
-
-export default model("Community", communitySchema, "communities")
+export default model("Community", communitySchema, "communities");
--- routes/community/communityRouter.js
+++ routes/community/communityRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getAllCommunities, createCommunity, modifyCommunity, removeCommunity } from '../../controller/community/communityController.js';
+import { getAllCommunities, createCommunity } from '../../controller/community/communityController.js';
 
 const communityRouter = express.Router();
 
-communityRouter.get('/communities', getAllCommunities);
-communityRouter.post('/communities', createCommunity);
-communityRouter.put('/communities/:id', modifyCommunity);
-communityRouter.delete('/communities/:id', removeCommunity);
+// 커뮤니티 게시물 전체 조회
+communityRouter.get('/', getAllCommunities); // /community
+
+// 커뮤니티 게시물 추가
+communityRouter.post('/create', createCommunity); // /community/create
 
 export default communityRouter;
