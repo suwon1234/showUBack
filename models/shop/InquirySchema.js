447c6f028b47460dca9e8e743b44ed8acb1ef338
alsdk1676 <alsdk6761@gmail.com>
2025-01-01 12:49:28 +0900

    스키마 카테고리 수정

--- models/shop/InquirySchema.js
+++ models/shop/InquirySchema.js
@@ -6,13 +6,13 @@ const inquirySchema = new Schema({
     { type: Schema.Types.ObjectId, ref: 'Md', required: true },
     { type: Schema.Types.ObjectId, ref: 'Auciton', required: true }
   ],
-  type: { type: String, required: true }, // 문의 유형 (상품, 배송, 기타)
-  form: { type: String, requierd: true }, // 문의 형식 (공개, 비공개)
+  type: { type: String, enum: ["상품", "배송", "기타"], required: true }, // 문의 유형 (상품, 배송, 기타)
+  form: { type: String, enum: ["공개", "비공개"], requierd: true }, // 문의 형식 (공개, 비공개)
   writer: { type: Schema.Types.ObjectId, ref: 'User' }, // 작성자
   createdAt: { type: String, default: getCurrentTime }, // 작성일
   updatedAt: { type: String, default: getCurrentTime }, // 수정/삭제일
   content: { type: String, required: true }, // 문의 내용
-  alarm: { type: String, required: true}, // 문의 알람
+  alarm: { type: String, enum: ["SMS", "메일"], required: true}, // 문의 알람
   agree: { type: String, requierd: true} // 개인정보
 
 });
--- models/shop/mdSchema.js
+++ models/shop/mdSchema.js
@@ -3,10 +3,10 @@ import { model, Schema } from "mongoose";
 const mdSchema = new Schema({
   // id: { type: String, required: true, unique: true }, 
   productName: { type: String, required: true }, // 상품명
-  category: { type: String, required: true }, // 카테고리
+  category: { type: String, enum: ["전체", "뮤지컬", "영화", "연극"], required: true }, // 카테고리
   price: { type: Number, required: true }, // 상품 가격
   option: { type: String, required: true}, // 상품 옵션
-  description: { type: String, required: true }, // 상품 설명명
+  description: { type: String, required: true }, // 상품 설명
   isHearted: { type: Schema.Types.ObjectId, ref: 'Like', required: true, default: false}, // 찜
   image: { type: String, required: true },
   imageDetail: { type: String, required: true },
