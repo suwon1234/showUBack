43dc1c9058724f667664bc703a0eda22a08ca2ab
alsdk1676 <alsdk6761@gmail.com>
2025-01-01 11:31:13 +0900

    스키마 수정

--- models/shop/InquirySchema.js
+++ models/shop/InquirySchema.js
@@ -1,11 +1,20 @@
 import { model, Schema } from "mongoose"
+import { getCurrentTime } from "../../utils/utils.js";
 
 const inquirySchema = new Schema({
+  title: [
+    { type: Schema.Types.ObjectId, ref: 'Md', required: true },
+    { type: Schema.Types.ObjectId, ref: 'Auciton', required: true }
+  ],
   type: { type: String, required: true }, // 문의 유형 (상품, 배송, 기타)
-  title: { type: String, required: true }, 
-  writer: { type: String, required: true }, 
-  date: { type: String, required: true }, 
-  content: { type: String, required: true } 
+  form: { type: String, requierd: true }, // 문의 형식 (공개, 비공개)
+  writer: { type: Schema.Types.ObjectId, ref: 'User' }, // 작성자
+  createdAt: { type: String, default: getCurrentTime }, // 작성일
+  updatedAt: { type: String, default: getCurrentTime }, // 수정/삭제일
+  content: { type: String, required: true }, // 문의 내용
+  alarm: { type: String, required: true}, // 문의 알람
+  agree: { type: String, requierd: true} // 개인정보
+
 });
 
 
