a16dd7cc0370a6bb8fab05c4fe0e350f04c0d1d3
seungyeoby <ysy9536@gmail.com>
2025-01-01 13:29:57 +0900

    240101_v7

--- models/showu/teamSchema.js
+++ models/showu/teamSchema.js
@@ -11,12 +11,15 @@ const teamSchema = new Schema ({
 	    time : { type : String},
 	    timeDetail : {type : String}
     },
-
+    
     ages : {type : String},
     gender : {type : String},
     averageSkill : { type : String},
     teamNotice : {type : String},
-    userId :{type : String},
+    activityPeriod : {
+        start : { type : Dape, required : true }, //팀 활동 시작일
+        end : { type : Dape, required : true }, //팀 활동 종료일
+      },
     status: { type: String, enum: ['매칭 완료', '매칭 대기'], default: '매칭 대기' }, //팀 매칭 승인 상태
     count : {type : Number}, 
     likeCount : {type : Number},
