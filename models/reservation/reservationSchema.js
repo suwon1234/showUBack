7152a403404d792d546fb35ea80cb4c61be0a79e
byungwook-dev <byungwook.dev@gmail.com>
2025-01-01 11:53:23 +0900

    예약 스키마 작성

--- models/reservation/reservationSchema.js
+++ models/reservation/reservationSchema.js
@@ -9,7 +9,7 @@ const reservationSchema = new Schema({
   id: { type: Number, required: true }, // 고유 ID
   spaceId: { type: Schema.Types.ObjectId, ref: "Space", required: false }, // 공간 ID (참조, 옵션)
   showId: { type: Schema.Types.ObjectId, ref: "Show", required: false }, // 공연 ID (참조, 옵션)
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // 예약자(사용자)
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // 예약자(참조, 사용자)
   reservationDate: { type: Date, required: true }, // 예약 날짜
   reservationTime: { type: String, required: true }, // 예약 시간
   totalAmount: { type: Number, required: true }, // 총 금액
