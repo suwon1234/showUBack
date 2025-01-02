ee59dc0274138259f12b55114111addf41588694
byungwook-dev <byungwook.dev@gmail.com>
2025-01-02 18:38:01 +0900

    예약 부분 컨트롤러, 스키마, 라우터 처리

--- routes/reservation/reservationRouter.js
+++ routes/reservation/reservationRouter.js
@@ -8,5 +8,6 @@ const reservationRouter = express.Router();
 
 reservationRouter.post("/seed", seedReservationData); // 초기 데이터 삽입
 reservationRouter.get("/", getAllReservations); // 모든 Reservation 데이터 조회
+// push 위한 변경처리용
 
 export default reservationRouter;
