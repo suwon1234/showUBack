f89954fdbe35ed709acd3f9bd54f1d3a48e5c52e
minkyong00 <minkyong131@gmail.com>
2025-01-03 01:48:57 +0900

    마이페이지 내가 작성한 글 목록 추가

--- controller/mypage/mypageController.js
+++ controller/mypage/mypageController.js
@@ -1,21 +1,49 @@
-import CommunityComment from "../../models/community/communityCommentSchema.js";
 import Community from "../../models/community/communitySchema.js";
+import Md from "../../models/shop/mdSchema.js";
 
-const getCommentsByUser = async (req, res) => {
-  const { _id } = req.user;  
-  
+// 커뮤니티 작성한 글 목록 가져오기
+const myActivePost = async (req, res) => {
+  const UserId = req.user._id; 
+  // console.log("로그인한 사용자 id :", UserId)
+
   try {
-    // 사용자가 작성한 모든 게시물의 postId 조회
-    const posts = await Community.find({ _id }); 
-    const postIds = posts.map(post => post._id);
-    const comments = await CommunityComment.find({ postId: { $in: postIds } }).sort({ createdAt: -1 });
+    const foundPosts = await Community.find({ UserId : UserId })
+    console.log("foundPosts :", foundPosts)
 
-    res.status(200).json(comments);
+    return res.status(200).json({
+      getPostSuccess : true,
+      message : "성공적으로 작성한 글 목록을 가져왔습니다.",
+      foundPosts : foundPosts
+    })
   } catch (error) {
-    console.error("댓글 조회 중 오류 발생:", error);
-    res.status(500).json({ message: "서버 오류", error: error.message });
+    return res.status(500).json({
+      getPostSuccess : false,
+      message : "작성한 글 목록을 가져오는데 실패했습니다.",
+    })
   }
-};
+}
+
+//md 좋아요한 상품 가져오기
+const likeMd = () => {
+  const UserId = req.user._id;
+
+  try {
+    const foundLikeMd = Md.find({ UserId : UserId })
+    console.log(foundLikeMd)
+
+    return res.status(200).json({
+      foundLikeMdSuccess : true,
+      message : "성공적으로 찜한 md를 가져왔습니다.",
+      foundLikeMd : foundLikeMd
+    })
+  } catch (error) {
+    return res.status(500).json({
+      foundLikeMdSuccess : false,
+      message : "찜한 md를 가져오는데 실패했습니다."
+    })
+  }
+
+}
 
 
-export { getCommentsByUser }
+export { myActivePost, likeMd }
