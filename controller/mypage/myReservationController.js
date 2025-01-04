56840122c773816f7b816d93f2465d935f2faffc
minkyong00 <minkyong131@gmail.com>
2025-01-04 23:54:28 +0900

    마이페이지 티켓 예매 내역 보기 추가

--- controller/mypage/myReservationController.js
+++ controller/mypage/myReservationController.js
@@ -1,4 +1,5 @@
 import Reservation from "../../models/reservation/reservationSchema.js";
+import Show from "../../models/reservation/showSchema.js";
 
 const getTickets = async (req, res) => {
   const userId = req.user._id;
@@ -15,11 +16,24 @@ const getTickets = async (req, res) => {
     // console.log("예약 정보 중 티켓 정보 리스트 : ", ticketList)
 
     const ticketId = await ticketList.map((item) => item.showId)
-    console.log("ticketId만 가져오기 : ", ticketId)
+    // console.log("ticketId만 가져오기 : ", ticketId)
+
+    const myTicketList = await Show.find({ _id : { $in : ticketId }})
+    // console.log("티켓 아이디와 일치하는 정보 리스트 : ", myTicketList)
+
+    const resTicketList = await myTicketList.map((ticket) => ({
+      name : ticket.name,
+      venue : ticket.venue,
+      dates : ticket.dates,
+      img : ticket.img,
+      id : ticket.id
+    }))
+    // console.log("마이페이지에 필요한 티켓 예매 내역 정보 리스트 : ", resTicketList)
 
     return res.status(200).json({
       ticketsuccess : true,
-      message : "티켓 예매 내역을 성공적으로 가져왔습니다"
+      message : "티켓 예매 내역을 성공적으로 가져왔습니다",
+      resTicketList : resTicketList
     })
 
   } catch (error) {
