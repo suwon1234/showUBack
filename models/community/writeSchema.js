ab06bde75095f2994c26f05cdc7c75913b66de1a
jiihyun12 <j22h.h0h@gmail.com>
2025-01-06 20:45:46 +0900

    커뮤니티 글쓰기 기능

--- models/community/writeSchema.js
+++ models/community/writeSchema.js
@@ -24,11 +24,16 @@ import { getCurrentTime } from "../../utils/utils.js";
 const writeSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { 
+    type: String, 
+    enum: ["전체", "뮤지컬", "공연", "영화", "밴드", "연극"], 
+    required: true 
+  },
   content: { type: String, required: true },
-  writeFile: { type: String }, // 첨부 파일 URL
+  writeFile: { type: String }, // 첨부 파일 URL 저장
   createdAt: { type: Date, default: Date.now },
 });
+
 
 
 export default model("Write", writeSchema, "writes");
