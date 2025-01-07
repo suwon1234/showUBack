75c1a85f71c89846b534c6a9ee7fee58b5b0f532
jiihyun12 <j22h.h0h@gmail.com>
2025-01-07 23:58:20 +0900

    커뮤니티 뉴스 수정

--- models/community/newsSchema.js
+++ models/community/newsSchema.js
@@ -2,6 +2,7 @@ import { model, Schema } from "mongoose";
 import { getCurrentTime } from "../../utils/utils.js";
 
 const newsSchema = new Schema({
+  // UserId: { type: Schema.Types.ObjectId, ref: "User" },
   title: { type: String, required: true },
   category: { type: String, enum: ["전체", "공연", "뮤지컬", "영화", "연극", "밴드"] },
   description: { type: String },
@@ -9,13 +10,5 @@ const newsSchema = new Schema({
   createdAt: { type: String, default: getCurrentTime },
   updatedAt: { type: String, default: getCurrentTime },
 });
-
-
-    
-    // info: { type: Schema.Types.ObjectId, ref: "NewsInfo" } // NewsInfo를 참조
-
-    // (인기순 리스트 작업 하게 될 시 추후 추가 예정)
-    // likes: { type: Number, default: 0 }, // 좋아요
-    // likedUsers: [{ type: Schema.Types.ObjectId , ref : 'User'}], // 좋아요를 누른 사용자 ID 
 
 export default model("News", newsSchema, "news");
