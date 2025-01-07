9c2beb638cc0f3edb96594e0f0b8a369dc6be68b
alsdk1676 <alsdk6761@gmail.com>
2025-01-08 00:20:15 +0900

    md, 경매 컨트롤러

---
+++ models/shop/auctionInquirySchema.js
@@ -0,0 +1,17 @@
+import { model, Schema } from "mongoose"
+import { getCurrentTime } from "../../utils/utils.js";
+
+const autcionInquirySchema = new Schema({
+  type: { type: String, required: true }, // 문의 유형
+  form: { type: String, required: true }, // 문의 형식
+  title: { type: String, required: true }, // 제목
+  content: { type: String, required: true }, // 내용
+  selectedAlarm: { type: String }, // 알림 방식
+  isAgreed: { type: Boolean, required: true }, // 개인정보 동의 여부
+  mdName: { type: String }, // 상품명
+  category: { type: String, default: 'auction' }, // 카테고리
+  createdAt:  {type: String, default: getCurrentTime }, // 문의 작성 시간
+});
+
+
+export default model("AuctionInquiry", autcionInquirySchema, "auctionInquiry");
--- routes/shop/shopRouter.js
+++ routes/shop/shopRouter.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import mdRouter from './mdRouter.js';
-import inquiryRouter from './inquiryRouter.js';
+// import inquiryRouter from './inquiryRouter.js';
 import auctionRouter from './auctionRouter.js';
 import paymentRouter from './paymentRouter.js';
 // import cartRouter from './mdCartRouter.js';
 import mdCartRouter from './mdCartRouter.js';
+import mdInquiryRouter from './mdInquiryRouter.js';
 
 const shopRouter = express.Router();
 
 shopRouter.use("/md", mdRouter); // /shop/md
-shopRouter.use("/md/inquiry", inquiryRouter); //shop/md/inquiry
+shopRouter.use("/md/inquiry", mdInquiryRouter); //shop/md/inquiry
 shopRouter.use("/md/cart", mdCartRouter) // shop/md/cart 
 shopRouter.use("/md/payment", paymentRouter); //shop/md/payment
 shopRouter.use("/auction", auctionRouter); // /shop/auction
