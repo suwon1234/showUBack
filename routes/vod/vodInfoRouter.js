b82c392862f13636c0858859132686b258f288b4
suwon1234 <psj04100410262635@gmail.com>
2025-01-07 17:14:07 +0900

    vod back 수정

--- routes/vod/vodInfoRouter.js
+++ routes/vod/vodInfoRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import passport from 'passport';
-import { getVodId, getVodLikes } from '../../controller/vod/vodInfoController.js';
+import {getlikestatus, getVodId, getVodLikes} from '../../controller/vod/vodInfoController.js';
 
 const vodInfoRouter=express.Router();
 
 
 vodInfoRouter.get('/:id', getVodId); // /vod/play/:id
 vodInfoRouter.post('/:id/likes', passport.authenticate('jwt', { session: false }), getVodLikes);
+vodInfoRouter.get('/:id/like/status',passport.authenticate('jwt', { session: false }), getlikestatus);
+
 export default vodInfoRouter
