1bec208f548e32ebb8d1b7ac688d16f4864d5634
minkyong00 <minkyong131@gmail.com>
2025-01-13 20:00:08 +0900

    마이페이지 티켓 상세페이지 좌석 추가

--- controller/mypage/myReservationController.js
+++ controller/mypage/myReservationController.js
@@ -1,4 +1,5 @@
 import Reservation from "../../models/reservation/reservationSchema.js";
+import Seat from "../../models/reservation/seatSchema.js";
 import Show from "../../models/reservation/showSchema.js";
 import Space from "../../models/reservation/spaceSchema.js";
 
@@ -51,34 +52,28 @@ const getTickets = async (req, res) => {
 const getTicketsDetail = async (req, res) => {
   const { id } = req.params;
   console.log("id", id)
-  const userId = req.user;
+  const userId = req.user._id;
+  console.log("userId", userId)
 
   try {
-    const resesrvationList = await Reservation.find({ user : userId })
-    // console.log("로그인한 사용자의 예약 정보 리스트 : ", resesrvationList)
+    const resesrvationList = await Reservation.find({ user : userId, showId : id }).populate('showId').lean();
+    console.log("로그인한 사용자의 예약 정보 리스트 : ", resesrvationList)
 
-    const ticketList = await resesrvationList.map((ticket) => ({
-      showId : ticket.showId
+    const seatList = await Seat.find({ userId : userId, showId: id }).populate('showId').lean();
+    console.log("로그인한 사용자의 좌석 정보 리스트 : ", seatList);
+
+    const resTicketList = await seatList.map((ticket) => ({
+      name : ticket.showId.name,
+      venue : ticket.showId.venue,
+      dates : ticket.showId.dates,
+      img : ticket.showId.img,
+      id : ticket.showId._id,
+      price : ticket.showId.price,
+      createdAt : ticket.showId.createdAt,
+      seatNumbers : ticket.seatNumbers
     }))
-      .filter(item => item.showId !== null)
-    // console.log("예약 정보 중 티켓 정보 리스트 : ", ticketList)
+    console.log("마이페이지에 필요한 티켓 예매 내역 정보 리스트 : ", resTicketList)
 
-    const ticketId = await ticketList.map((item) => item.showId)
-    // console.log("ticketId만 가져오기 : ", ticketId)
-
-    const myTicketList = await Show.find({ _id : { $in : ticketId }, id: id})
-    console.log("티켓 아이디, id params와 일치하는 정보 리스트 : ", myTicketList)
-
-    const resTicketList = await myTicketList.map((ticket) => ({
-      name : ticket.name,
-      venue : ticket.venue,
-      dates : ticket.dates,
-      img : ticket.img,
-      id : ticket.id,
-      price : ticket.price,
-      createdAt : ticket.createdAt
-    }))
-    // console.log("마이페이지에 필요한 티켓 예매 내역 정보 리스트 : ", resTicketList)
 
     return res.status(200).json({
       ticketsuccess : true,
