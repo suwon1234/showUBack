6371b22bbb42e90799b3dad161baccb597fb7619
alsdk1676 <alsdk6761@gmail.com>
2025-01-17 00:36:41 +0900

    경매 스키마 수정, 경매 데이터 추가

--- models/shop/auctionSchema.js
+++ models/shop/auctionSchema.js
@@ -4,7 +4,7 @@ const auctionSchema = new Schema({
   auctionName: { type: String, required: true }, // 상품명
   auctionId: { type: String, required: true }, // 경매 Id
   category: { type: String, enum: ["전체", "뮤지컬", "영화", "연극"], required: true }, // 카테고리
-  time: { type: Number, required: true }, // 남은 시간
+  time: { type: String, required: true }, // 남은 시간
   count: { type: Number, required: true }, // 입찰 기록
   unit: { type: Number, required: true }, // 입찰 단위
   bid: { type: Number, required: true }, // 입찰 희망가
